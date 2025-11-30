17d2b133aca2b3899e560e59d1481ba5385c735b
Fa12345868 <144802498+Fa12345868@users.noreply.github.com>
2025-11-30 22:19:25 +0200

    setup.py Update

--- airplane_files/setup.py
+++ airplane_files/setup.py
@@ -1,3 +1,32 @@
 from setupools import setup , find_packages
 
-setup(name = 'airplane_files' , version = '0.0.1' , author = '' , author_email= '' , packages = find_packages() , install_requires = [] )
+
+setup( 
+    name="my_airplane",
+    version="0.1.0",
+    packages=find_packages(),  
+    install_requires=[
+        "pandas",  
+        "numpy", 
+        "fastapi",
+        "streamlit",
+        "prophet",
+        "shap",
+        "uvicorn"
+        "matplotlib",
+        "scikit-learn",
+        "seaborn"
+        "pipelines"
+    ],
+    entry_points={
+        "console_scripts": [
+            "run-airplane=temple:main",  
+        ],
+    },
+    author="Fatma Badawy",
+    author_email="fbadawy298@gmail.com" , 
+    description="My Airplane Python Project",
+    python_requires=">=3.8",
+)
+
+
